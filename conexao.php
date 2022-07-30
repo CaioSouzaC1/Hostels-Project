160668b845d873ad8315acbe67f439b8c930cd39
CaioSouzaC1 <108900609+CaioSouzaC1@users.noreply.github.com>
2022-07-30 20:35:41 -0300

    0.6
    Correções Conexão

--- conexao.php
+++ conexao.php
@@ -6,6 +6,6 @@ $db = "db_hostel_project";
 
 $conexao = new mysqli($host,$user,$senha,$db);
 if ($conexao->connect_errno){
-    echo "Erro na conexao" . $connect_errno;
+    echo "Erro na conexao" . $conexao->connect_errno;
 }
 ?>
